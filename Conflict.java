aa8783fd36728ac229ef3b3f688fac0561d2439a
rui-murayama <r.murayama1811@morijyobi.ac.jp>
2019-06-04 11:32:17 +0900

    [modify]Conflict.javaの変更

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,14 @@
-public class Conflict{
-	public static void main(String[] args){
-	System.out.println("HelloWorld");
+
+import java.util.Random;
+
+public class Conflict {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		for(int i=0;i<10;i++){
+			int randomValue=random.nextInt(100)+1;
+				System.out.println(randomValue);
+		}
 	}
+
 }
